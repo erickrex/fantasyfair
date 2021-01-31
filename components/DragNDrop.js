460ea43c518189be8c1a7dce08d39b083ab12221
erickrex <59777190+erickrex@users.noreply.github.com>
2021-02-01 00:00:02 +0100

    Ordered list

--- components/DragNDrop.js
+++ components/DragNDrop.js
@@ -24,6 +24,21 @@ function DragNDrop({data}) {
         },0)       
     }
     
+    const handleDragEnter = (e, targetItem) => {
+        console.log('Entering a drag target', targetItem)
+        if (dragItemNode.current !== e.target) {
+            console.log('Target is NOT the same as dragged item')
+            setList(oldList => {
+                let newList = JSON.parse(JSON.stringify(oldList))
+                newList[targetItem.grpI].items.splice(targetItem.itemI, 0, newList[dragItem.current.grpI].items.splice(dragItem.current.itemI,1)[0])
+                dragItem.current = targetItem;
+                localStorage.setItem('List', JSON.stringify(newList));
+                console.log({newList})
+                return newList
+            })
+        }
+    }    
+    
     if (list) {
         return (                
             
