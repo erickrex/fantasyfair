6361284c7f3c0ed90c0d5f7348f07b7c570cb85b
erickrex <59777190+erickrex@users.noreply.github.com>
2021-01-31 23:59:30 +0100

    Draggable items

---
+++ components/DragNDrop.js
@@ -0,0 +1,35 @@
+import React, {useState, useRef, useEffect} from 'react'
+
+function DragNDrop({data}) {
+
+    const [list, setList] = useState(data); 
+    const [dragging, setDragging] = useState(false);
+
+    useEffect(() => {
+        setList(data);
+    }, [setList, data])
+
+    const dragItem = useRef();
+    const dragItemNode = useRef();
+    
+    const handletDragStart = (e, item) => {
+        console.log('Starting to drag', item)
+
+        dragItemNode.current = e.target;
+        dragItemNode.current.addEventListener('dragend', handleDragEnd)
+        dragItem.current = item;
+
+        setTimeout(() => {
+            setDragging(true); 
+        },0)       
+    }
+    
+    if (list) {
+        return (                
+            
+        )
+    } else { return null}
+
+}
+
+export default DragNDrop;
