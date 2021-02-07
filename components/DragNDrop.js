c9bb52ad60037fd0a76c15944da9437f6680676a
erickrex <59777190+erickrex@users.noreply.github.com>
2021-02-08 00:00:31 +0100

    Missing div

--- components/DragNDrop.js
+++ components/DragNDrop.js
@@ -67,7 +67,7 @@ function DragNDrop({data}) {
                   
                   </div>  
                 ))}
-                
+                </div>
             ))}
             
             </div>
