b11e50534cf65c8f12a9072d572c88a82d4011db
erickrex <59777190+erickrex@users.noreply.github.com>
2021-02-12 23:56:38 +0100

    Data via json object

---
+++ data/Nxt_2021_02_14.js
@@ -0,0 +1,38 @@
+export const Questions = [
+    {
+      prompt: "Dusty Rhodes Cup Men's finals",
+      contenders:   [
+                        {challenger: "MSK"}, 
+                        {challenger: "Roman_Reigns"}
+                    ]
+    },
+    {
+      prompt: "North American Championship",
+      contenders:   [
+                    {challenger: "Johny Gargano"}, 
+                    {challenger: "Kushida"}
+                    ]
+    },
+    {
+      prompt: "Dusty Rhodes Cup Women's finals",
+      contenders:   [
+                    {challenger: "Dakota Kai & Raquel Gonzalez"}, 
+                    {challenger: "Shotzi Blackheart & Ember Moon"}
+                    ]
+    },
+    {
+    prompt: "NXT Women's Championship",
+    contenders: [
+                {challenger: "Io Shirai"}, 
+                {challenger: "Toni Storm"},
+                {challenger: "Mercedes Martinez"},
+                ]
+    },
+    {
+    prompt: "NXT Championship",
+    contenders: [
+                {challenger: "Finn Balor"}, 
+                {challenger: "Pete Dunne"},
+                ]    
+    }
+  ];
