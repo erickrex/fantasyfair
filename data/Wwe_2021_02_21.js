cd3861c83d936a2512730c56e9aba64f0de37751
erickrex <59777190+erickrex@users.noreply.github.com>
2021-02-21 23:52:28 +0100

    Card of PPVs

---
+++ data/Wwe_2021_02_21.js
@@ -0,0 +1,24 @@
+export const Card = [
+  {
+    matchName: "United States Championship",
+    contenders:   [
+                      {name: "Bobby Lashley", status: "champion"}, 
+                      {name: "Riddle", status: "challenger"},
+                      {name: "Keith Lee", status: "challenger"}
+                  ]
+  },
+  {
+    matchName: "Women's Tag Team Championship",
+    contenders:   [
+                  {name: "Nia Jax & Shayna Baszler", status: "champion"}, 
+                  {name: "Sasha Banks & Bianca Belair", status: "challenger"}
+                  ]   
+  },
+  {
+    matchName: "Raw Women's Championship",
+    contenders:   [
+                  {name: "Asuka", status: "champion"}, 
+                  {name: "TBD", status: "challenger"}
+                  ]
+  }
+];
