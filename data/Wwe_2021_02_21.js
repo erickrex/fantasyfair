8a2a2e773f4147ededcd8ca1aa4b03d4c40a6de3
erickrex <59777190+erickrex@users.noreply.github.com>
2021-03-04 23:57:54 +0100

    Update Wwe_2021_02_21.js

--- data/Wwe_2021_02_21.js
+++ data/Wwe_2021_02_21.js
@@ -1,24 +1,65 @@
-export const Card = [
-  {
-    matchName: "United States Championship",
-    contenders:   [
+import { useMachine } from '@xstate/react';
+import { Machine } from 'xstate';
+
+const stepMachine = Machine({
+  id: 'step',
+  initial: 'one',
+  states: {
+    one: {
+      meta: {
+        matchName: "United States Championship",
+        contenders:   [
                       {name: "Bobby Lashley", status: "champion"}, 
                       {name: "Riddle", status: "challenger"},
                       {name: "Keith Lee", status: "challenger"}
                   ]
-  },
-  {
-    matchName: "Women's Tag Team Championship",
-    contenders:   [
-                  {name: "Nia Jax & Shayna Baszler", status: "champion"}, 
-                  {name: "Sasha Banks & Bianca Belair", status: "challenger"}
-                  ]   
-  },
-  {
-    matchName: "Raw Women's Championship",
-    contenders:   [
-                  {name: "Asuka", status: "champion"}, 
-                  {name: "TBD", status: "challenger"}
+      },
+      on: { NEXT: 'two' }
+    },
+    two: {
+      meta: {
+        matchName: "24/7 Championship",
+        contenders:   [
+                      {name: "Asuka", status: "champion"}, 
+                      {name: "Orton", status: "challenger"},
+                      {name: "Keith Lee", status: "challenger"}
                   ]
+      },
+      on: { NEXT: 'three', PREV: 'one' }
+    },
+    three: {
+      meta: {
+        matchName: "Womens Championship",
+        contenders:   [
+                      {name: "Bobby Lashley", status: "champion"}, 
+                      {name: "Riddle", status: "challenger"},
+                      {name: "Keith Lee", status: "challenger"}
+                  ]
+      },
+      on: { NEXT: 'four', PREV: 'two' }
+    },
+    four: {
+      meta: {
+        matchName: "North American Championship",
+      contenders:   [
+                    {name: "Johnny Gargano", status: "champion"}, 
+                    {name: "Kushida", status: "challenger"}
+                    ]
+      },
+      on: { NEXT: 'five', PREV: 'three' }
+    },
+    five: {
+      meta: {
+        matchName: "United States Championship",
+        contenders:   [
+                      {name: "Bobby Lashley", status: "champion"}, 
+                      {name: "Riddle", status: "challenger"},
+                      {name: "Keith Lee", status: "challenger"}
+                  ]
+      },
+      type: 'final'
+    }
   }
-];
+});
+
+console.log(stepMachine.transition('one', 'NEXT').value);
